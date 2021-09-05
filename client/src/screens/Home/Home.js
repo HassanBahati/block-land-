6e617de7ace8403b38daf4fbe29c9a1acddcad0a
HassanBahati <mukisabahati@gmail.com>
2021-09-05 19:20:08 +0300

    added knowonwer

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,10 +1,12 @@
 import Layout from '../../components/Layout/Layout.js'
+import KnowOwner from '../../components/ui/KnowOwner.js'
 import LandingSection from '../../components/ui/LandingSection'
 
 const Home = () => {
     return (
         <Layout>
 <LandingSection/>
+<KnowOwner/>
         </Layout>
     )
 }
