574a16c14cdadbdda989ede5decd9d97b0068473
HassanBahati <mukisabahati@gmail.com>
2021-09-05 18:59:58 +0300

    added owners section

---
+++ client/src/components/ui/KnowOwner.js
@@ -0,0 +1,19 @@
+const KnowOwner = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg">
+          <h5>DON'T BE CHEATED</h5>
+          <h3>KNow the real Owners</h3>
+          <p>
+            We affirm real owners and onboard properties attached to their real
+            owners onto a BlockChain network thereby overcoming fraud.{" "}
+          </p>
+        </div>
+        <div className="col-lg"></div>
+      </div>
+    </div>
+  );
+};
+
+export default KnowOwner;
