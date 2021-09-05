a9bdcbe8992b4394a6e3b452e7bf094c5490901f
HassanBahati <mukisabahati@gmail.com>
2021-09-05 18:38:20 +0300

    added lalnding jsx

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,9 +1,10 @@
 import Layout from '../../components/Layout/Layout.js'
+import LandingSection from '../../components/ui/LandingSection'
 
 const Home = () => {
     return (
         <Layout>
-
+<LandingSection/>
         </Layout>
     )
 }
