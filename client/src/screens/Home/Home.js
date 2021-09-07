313d59eb734aa47472ea6860b2a5b8799e07ced7
HassanBahati <mukisabahati@gmail.com>
2021-09-07 21:18:08 +0300

    fix bug

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,14 +1,14 @@
-import Layout from '../../components/Layout/Layout.js'
-import KnowOwner from '../../components/ui/KnowOwner.js'
-import LandingSection from '../../components/ui/LandingSection'
+import Layout from "../../components/Layout/Layout.js";
+import KnowOwner from "../../components/ui/KnowOwner.js";
+import LandingSection from "../../components/ui/LandingSection";
 
 const Home = () => {
-    return (
-        <Layout>
-<LandingSection/>
-<KnowOwner/>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <LandingSection />
+      <KnowOwner />
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
