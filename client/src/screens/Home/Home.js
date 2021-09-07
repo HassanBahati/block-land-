54a66e5086ffb5aa3ce28d080f630097f06c01b4
HassanBahati <mukisabahati@gmail.com>
2021-09-07 21:43:35 +0300

    added contact form

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layout/Layout.js";
 import About from "../../components/ui/About.js";
+import Contact from "../../components/ui/Contact.js";
 import KnowOwner from "../../components/ui/KnowOwner.js";
 import LandingSection from "../../components/ui/LandingSection";
 import Properties from "../../components/ui/Properties.js";
@@ -11,6 +12,7 @@ const Home = () => {
       <About/>
       <KnowOwner />
       <Properties/>
+      <Contact/>
     </Layout>
   );
 };
